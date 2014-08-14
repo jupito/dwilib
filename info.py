b7d928b473ec6b1eb60707783842d78b9a9ecdec
Jussi Toivonen <jupito@iki.fi>
2014-08-14 17:09:53 +0300

    Print sum. Print description only if it exists.

--- info.py
+++ info.py
@@ -10,12 +10,13 @@ from dwi import asciifile
 for filename in sys.argv[1:]:
     af = asciifile.AsciiFile(filename)
     print filename
-    print af.d['description']
+    if af.d.has_key('description'):
+        print af.d['description']
     params = af.params()
     for i, a in enumerate(af.a.T):
         d = dict(i=i, param=params[i],
                 median=np.median(a), mean=a.mean(), var=a.var(),
-                min=a.min(), max=a.max())
+                min=a.min(), max=a.max(), sum=sum(a))
         print '{i}\t{param}'\
                 '\t{median:g}\t{mean:g}\t{var:g}'\
-                '\t{min:g}\t{max:g}'.format(**d)
+                '\t{min:g}\t{max:g}\t{sum:g}'.format(**d)
