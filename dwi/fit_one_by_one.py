9ad6051ec968312c2578cb3fbe5a80f203da3b54
Jussi Toivonen <jupito@iki.fi>
2014-08-20 17:37:57 +0300

    Moved around some functions for readability

--- dwi/fit_one_by_one.py
+++ dwi/fit_one_by_one.py
@@ -3,16 +3,15 @@
 import numpy as np
 from leastsqbound import leastsqbound
 
-def fit_curve(f, xdata, ydata, guess, bounds):
-    """Fit a curve to data."""
-    residual = lambda p, x, y: f(p, x) - y
-    params, ier = leastsqbound(residual, guess, args=(xdata, ydata),
-            bounds=bounds)
-    if 0 < ier < 5:
-        err = rmse(f, params, xdata, ydata)
-    else:
-        err = np.inf
-    return params, err
+def fit_curves_mi(f, xdata, ydatas, guesses, bounds, out_pmap):
+    """Fit curves to data with multiple initializations."""
+    for i, ydata in enumerate(ydatas):
+        params, err = fit_curve_mi(f, xdata, ydata, guesses, bounds)
+        out_pmap[i, -1] = err
+        if np.isfinite(err):
+            out_pmap[i, :-1] = params
+        else:
+            out_pmap[i, :-1].fill(np.nan)
 
 def fit_curve_mi(f, xdata, ydata, guesses, bounds):
     """Fit a curve to data with multiple initializations."""
@@ -26,15 +25,16 @@ def fit_curve_mi(f, xdata, ydata, guesses, bounds):
             best_err = err
     return best_params, best_err
 
-def fit_curves_mi(f, xdata, ydatas, guesses, bounds, out_pmap):
-    """Fit curves to data with multiple initializations."""
-    for i, ydata in enumerate(ydatas):
-        params, err = fit_curve_mi(f, xdata, ydata, guesses, bounds)
-        out_pmap[i, -1] = err
-        if np.isfinite(err):
-            out_pmap[i, :-1] = params
-        else:
-            out_pmap[i, :-1].fill(np.nan)
+def fit_curve(f, xdata, ydata, guess, bounds):
+    """Fit a curve to data."""
+    residual = lambda p, x, y: f(p, x) - y
+    params, ier = leastsqbound(residual, guess, args=(xdata, ydata),
+            bounds=bounds)
+    if 0 < ier < 5:
+        err = rmse(f, params, xdata, ydata)
+    else:
+        err = np.inf
+    return params, err
 
 def rmse(f, p, xdata, ydata):
     """Root-mean-square error."""
