af04e74fd480a1fa9101eeffaa3c1f58cb55b5b2
Jussi Toivonen <jupito@iki.fi>
2014-12-12 02:41:14 +0200

    Added task for find_roi2.py

--- dodo.py
+++ dodo.py
@@ -185,5 +185,14 @@ def task_select_roi_auto():
             mask = 'auto'
             yield get_task_select_roi(case, scan, 'Mono', 'ADCm', subwin, mask)
 
+def task_select_roi_auto2():
+    """Select automatic ROIs from the pmap DICOMs."""
+    for sample in SAMPLES_ALL:
+        for scan in sample['scans']:
+            case = sample['case']
+            subwin = None
+            mask = 'auto2'
+            yield get_task_select_roi(case, scan, 'Mono', 'ADCm', subwin, mask)
+
 SAMPLES_ALL = dwi.util.read_sample_list('samples_all.txt')
 SUBWINDOWS = dwi.util.read_subwindows('subwindows.txt')
