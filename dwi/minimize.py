b24ed96e19a1779ecff1ffc12b238fdb1808babf
Jussi Toivonen <jupito@iki.fi>
2014-12-17 17:57:26 +0200

    Some minimization functions for special needs

---
+++ dwi/minimize.py
@@ -0,0 +1,101 @@
+"""Minimization functions."""
+
+import numpy as np
+import scipy.optimize
+
+epsilon = np.sqrt(np.finfo(np.float).eps)
+
+def gradient(f, x, args=[], eps=epsilon):
+    """Approximate gradient of f at x."""
+    return scipy.optimize.approx_fprime(x, f, eps, *args)
+
+def gradient_descent(f, df=None, init=[0.0], step=0.5, args=[], maxiter=10000,
+        accuracy=1.0e-15):
+    """Minimize f by gradient descent."""
+    assert 0 < step < 1
+    assert maxiter > 0
+    assert accuracy >= 0
+    init = np.atleast_1d(np.asarray(init, dtype=float))
+    x = init
+    warnflag = 1
+    for i in xrange(maxiter):
+        if df:
+            dfx = df(x, args)
+        else:
+            dfx = gradient(f, x, args)
+        x_prev = x
+        x = x - dfx*step
+        #print x, f(x, *args)
+        #step *= 0.90
+        #if (abs(x-x_prev) < accuracy).all():
+        #if abs(f(x, *args)-f(x_prev, *args)) < accuracy:
+        #if abs(f(x, *args)-f(x_prev, *args)) < accuracy:
+        if abs(f(x, *args)-f(x_prev, *args)) < epsilon:
+            warnflag = 0
+            break
+    d = dict(x=x, y=f(x, *args), grad=dfx, nit=i+1, init=init, step=step,
+            args=args, maxiter=maxiter, accuracy=accuracy, warnflag=warnflag)
+    return d
+
+def gradient_descent_mi(f, inits, **kwargs):
+    """Gradient descent with multiple initializations."""
+    d_min = None
+    for init in inits:
+        d = gradient_descent(f, init=init, **kwargs)
+        if not d_min or d['y'] < d_min['y']:
+            d_min = d
+    return d_min
+
+def line_search(f, x, args, rho=0.5, c=0.5, alpha0=0.5):
+    """Backtracking line search. Nodecal & Wright 1999 pg41."""
+    alpha = alpha0
+    dfx = gradient(f, x, args)
+    p = -dfx/np.linalg.norm(dfx)
+    while f(x+alpha*p, *args) > f(x, *args) + c*alpha*np.dot(dfx, p):
+        alpha = rho*alpha
+    return alpha
+
+def fletcher_reeves(x, x_):
+    return np.dot(x, x) / np.dot(x_, x_)
+
+def polak_ribiere(x, x_):
+    return np.dot(x, (x-x_)) / np.dot(x_, x_)
+
+def cg(f, x0, args=[], maxiter=1000):
+    """Nonlinear conjugate gradient method. Nocedel & Wright 1999 pg120."""
+    x0 = np.atleast_1d(np.asarray(x0, dtype=float))
+    x = x0
+    dfx = gradient(f, x, args)
+    p = -dfx
+    k = 0
+    while dfx.any() and k < maxiter:
+        dfx_prev = dfx
+        alpha = line_search(f, x, args)
+        x = x + alpha*p
+        dfx = gradient(f, x, args)
+        #beta = fletcher_reeves(dfx, dfx_prev)
+        beta = max(0, polak_ribiere(dfx, dfx_prev))
+        p = -dfx + beta*p
+        k = k+1
+    d = dict(x=x, y=f(x, *args), nit=k)
+    return d
+
+def cg_old(f, x0, args=[], maxiter=1000):
+    x0 = np.atleast_1d(np.asarray(x0, dtype=float))
+    x = x0
+    dfx = gradient(f, x, args)
+    dx = -dfx
+    s = dx
+    alpha = line_search(f, x, args)
+    x = x + alpha*dx
+    for i in xrange(maxiter):
+        dfx = gradient(f, x, args)
+        dx_prev = dx
+        dx = -dfx
+        beta = max(0, polak_ribiere(dx, dx_prev))
+        s_prev = s
+        s = dx + beta*s_prev
+        alpha = line_search(f, x, args)
+        x = x + alpha*s
+    d = dict(x=x, y=f(x, *args), nit=i+1)
+    return d
