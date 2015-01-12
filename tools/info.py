7291ccdfdae43b39b22f9a4e73b9a276a7a4218c
Jussi Toivonen <jupito@iki.fi>
2015-01-12 21:57:25 +0200

    Refactoring, arg handling

--- tools/info.py
+++ tools/info.py
@@ -1,24 +1,45 @@
 #!/usr/bin/env python2
 
-# Print information columns of number values.
+"""Print information about columns of number values."""
 
+import argparse
 import sys
 import numpy as np
 
 from dwi import asciifile
 from dwi import util
 
-for filename in sys.argv[1:]:
+def parse_args():
+    """Parse command-line arguments."""
+    p = argparse.ArgumentParser(description=__doc__)
+    p.add_argument('input', metavar='FILENAME', nargs='+', default=[],
+            help='input ASCII files')
+    p.add_argument('-v', '--verbose',
+            action='count',
+            help='increase verbosity')
+    p.add_argument('--basic', '-b', action='store_true',
+            help='show median, mean, variance, min, max, sum')
+    p.add_argument('--five', '-f', action='store_true',
+            help='show Tukey\'s five numbers (min, q1, median, q3, max)')
+    args = p.parse_args()
+    return args
+
+args = parse_args()
+for filename in args.input:
     af = asciifile.AsciiFile(filename)
-    print filename
-    if af.d.has_key('description'):
-        print af.d['description']
+    if args.verbose:
+        print filename
+        if af.d.has_key('description'):
+            print af.d['description']
     params = af.params()
     for i, a in enumerate(af.a.T):
-        d = dict(i=i, param=params[i],
-                median=np.median(a), mean=a.mean(), var=a.var(),
-                min=a.min(), max=a.max(), sum=sum(a))
-        print '{i}\t{param}'\
-                '\t{median:g}\t{mean:g}\t{var:g}'\
-                '\t{min:g}\t{max:g}\t{sum:g}'.format(**d)
-        print util.fivenum(a)
+        d = dict(i=i, param=params[i])
+        if args.basic:
+            d.update(mean=a.mean(), std=a.std(), var=a.var(), sum=sum(a))
+            print '{i}\t{param}'\
+                    '\t{mean:g}\t{std:g}\t{var:g}'\
+                    '\t{sum:g}'.format(**d)
+        if args.five:
+            d.update(util.fivenumd(a))
+            print '{i}\t{param}'\
+                    '\t{min:g}\t{q1:g}\t{median:g}\t{q3:g}\t{max:g}'.format(**d)
