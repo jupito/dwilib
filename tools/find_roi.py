b9740ea71095204da224adb8df6d58a484abe590
Jussi Toivonen <jupito@iki.fi>
2015-02-02 16:50:22 +0200

    Broke long line

--- tools/find_roi.py
+++ tools/find_roi.py
@@ -291,8 +291,10 @@ for d in data:
             prostate_mask=d['prostate_mask'], sidemin=sidemin, sidemax=sidemax,
             n_rois=n_rois))
     print '{case} {scan}: Optimal ROI at {roi_corner}'.format(**d)
-    draw(d, args.param, args.outfig or OUT_IMAGE_DIR+'/{case}_{scan}.png'.format(**d))
-    write_mask(d, args.outmask or OUT_MASK_DIR+'/{case}_{scan}_auto.mask'.format(**d))
+    draw(d, args.param, args.outfig or
+            OUT_IMAGE_DIR+'/{case}_{scan}.png'.format(**d))
+    write_mask(d, args.outmask or
+            OUT_MASK_DIR+'/{case}_{scan}_auto.mask'.format(**d))
 
 #if args.verbose:
 #    for i, p in enumerate(params):
