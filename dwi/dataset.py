7b367f366d69048cbb71a8287a9908c359add2a3
Jussi Toivonen <jupito@iki.fi>
2015-02-25 16:51:53 +0200

    Added functions to create dataset

--- dwi/dataset.py
+++ dwi/dataset.py
@@ -98,3 +98,57 @@ def read_dicom_pmaps(samplelist_file, patients_file, image_dir, subregion_dir,
                     d['cancer_mask'].array.shape ==\
                     d['normal_mask'].array.shape
     return data
+
+def dataset_read_samplelist(samplelist_file, cases=[], scans=[]):
+    """Create a new dataset from a sample list file, optionally including only
+    mentioned cases and scans."""
+    samples = dwi.util.read_sample_list(samplelist_file)
+    data = []
+    for sample in samples:
+        case = sample['case']
+        if cases and not case in cases:
+            continue
+        for scan in sample['scans']:
+            if scans and not scan in scans:
+                continue
+            data.append(dict(case=case, scan=scan))
+    return data
+
+def dataset_read_patientinfo(data, patients_file):
+    """Add patient info to dataset."""
+    patientsinfo = dwi.patient.read_patients_file(patients_file)
+    for d in data:
+        d['score'] = dwi.patient.get_patient(patientsinfo, d['case']).score
+
+def dataset_read_subregions(data, subregion_dir):
+    """Add subregions to dataset."""
+    for d in data:
+        d['subregion'] = read_subregion(subregion_dir, d['case'], d['scan'])
+
+def dataset_read_pmaps(data, image_dir, param):
+    """Add pmaps to dataset."""
+    for d in data:
+        image = read_dicom_pmap(image_dir, d['case'], d['scan'], param)
+        if 'subregion' in d:
+            d['original_shape'] = image.shape
+            image = dwi.util.crop_image(image, d['subregion']).copy()
+        d['image'] = image
+
+def dataset_read_prostate_masks(data, prostate_mask_dir):
+    """Add prostate masks to dataset."""
+    for d in data:
+        mask = read_prostate_mask(prostate_mask_dir, d['case'], d['scan'])
+        if 'subregion' in d:
+            mask = mask.crop(d['subregion'])
+        d['prostate_mask'] = mask
+
+def dataset_read_roi_masks(data, roi_mask_dir):
+    """Add ROI masks to dataset."""
+    for d in data:
+        masks = read_roi_masks(roi_mask_dir, d['case'], d['scan'])
+        cancer_mask, normal_mask = masks['ca'], masks['n']
+        if 'subregion' in d:
+            cancer_mask = cancer_mask.crop(d['subregion'])
+            normal_mask = normal_mask.crop(d['subregion'])
+        d['cancer_mask'] = cancer_mask
+        d['normal_mask'] = normal_mask
