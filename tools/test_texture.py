11029b34b92dd75a850447b6553143f28ad5dfd8
Jussi Toivonen <jupito@iki.fi>
2015-02-26 22:34:42 +0200

    Lots of stuff

--- tools/test_texture.py
+++ tools/test_texture.py
@@ -13,6 +13,7 @@ import dwi.util
 import dwi.patient
 import dwi.dwimage
 import dwi.mask
+import dwi.plot
 import dwi.texture
 
 def parse_args():
@@ -30,6 +31,44 @@ def parse_args():
             help='window step size')
     args = p.parse_args()
     return args
+
+def get_roi_slices(data):
+    for d in data:
+        img = d['image']
+        d['cancer_slice'] = d['cancer_mask'].get_masked_slice(img)
+        d['normal_slice'] = d['normal_mask'].get_masked_slice(img)
+
+def get_lbpf(img):
+    lbp, lbp_freq, n_patterns = dwi.texture.get_lbp_freqs(img)
+    return lbp_freq
+    #return lbp_freq[...,1:] # Drop non-uniform patterns
+
+def avg_lbpf_map(histogram_map):
+    """Return the average of multidimensional LPB frequency histogram map."""
+    histogram_map = np.asanyarray(histogram_map)
+    n_patterns = histogram_map.shape[-1]
+    histograms = histogram_map.reshape(-1, n_patterns)
+    return np.mean(histograms, axis=0)
+
+def get_lbpfs(data):
+    """Get LBP frequency histograms."""
+    for d in data:
+        d['cancer_slice_lbpf'] = get_lbpf(d['cancer_slice'][...,0])
+        d['cancer_slice_lbpf_avg'] = avg_lbpf_map(d['cancer_slice_lbpf'])
+        d['cancer_lbpf_avg'] = avg_lbpf_map(get_lbpf(d['cancer_roi']))
+        d['normal_lbpf_avg'] = avg_lbpf_map(get_lbpf(d['normal_roi']))
+
+def get_distances(data, model):
+    """Get LBP frequence histogram distances."""
+    for d in data:
+        sample = d['cancer_slice_lbpf']
+        #model = d['cancer_lbpf_avg']
+        print sample.shape
+        dist = np.zeros_like(sample[...,0])
+        for y, x in np.ndindex(dist.shape):
+            dist[y,x] = dwi.texture.lbpf_dist(sample[y,x], model,
+                    method='log-likelihood')
+        d['distance_map'] = dist
 
 def draw_props(img, title, win_step):
     pmap = dwi.texture.get_texture_pmap(img, win_step)
@@ -53,14 +92,31 @@ def draw_props(img, title, win_step):
     plt.savefig(outfile, bbox_inches='tight')
 
 
-data = dwi.dataset.dataset_read_samplelist('samples_train.txt', cases=args.cases)
+args = parse_args()
+
+print 'Reading dataset...'
+data = dwi.dataset.dataset_read_samplelist('samples_train.txt', cases=args.cases, scans=['1a', '2a'])
 dwi.dataset.dataset_read_patientinfo(data, 'patients.txt')
 dwi.dataset.dataset_read_subregions(data, 'bounding_box_100_10pad')
 dwi.dataset.dataset_read_pmaps(data, 'results_Mono_combinedDICOM', 'ADCm')
 #dwi.dataset.dataset_read_prostate_masks(data, 'masks_prostate')
 dwi.dataset.dataset_read_roi_masks(data, 'masks_rois', shape=(5,5))
-
+get_roi_slices(data)
 for d in data:
     print d['case'], d['scan'], d['score'], d['image'].shape
     #print d['subregion'], dwi.util.subwindow_shape(d['subregion'])
 
+print 'Calculating texture properties...'
+get_lbpfs(data)
+model = avg_lbpf_map([d['cancer_lbpf_avg'] for d in data])
+get_distances(data, model)
+
+print 'Plotting...'
+l = []
+for d in data:
+    imgs = [d['cancer_slice'][...,0], d['distance_map']]
+    l.append(imgs)
+imgs = [[d['cancer_slice'][...,0], d['distance_map']] for d in data]
+ylabels=[d['case'] for d in data]
+xlabels=['adc', 'dist']
+dwi.plot.show_images(l, ylabels, xlabels, outfile='dist.png')
