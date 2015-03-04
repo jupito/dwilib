fb401d88ac09cc4beb5dd25fd358295a4665b588
Jussi Toivonen <jupito@iki.fi>
2015-03-04 11:18:26 +0200

    Argument --pad for padding size

--- tools/masktool.py
+++ tools/masktool.py
@@ -18,24 +18,29 @@ def parse_args():
             help='input mask file')
     p.add_argument('--subregion', '-s', metavar='OUTFILE',
             help='output subregion file')
+    p.add_argument('--pad', metavar='I', type=int, default=10,
+            help='subregion padding size')
     args = p.parse_args()
     return args
+
+def write_subregion(mask, pad, filename):
+    """Write a subregion file from mask."""
+    bb = mask.bounding_box(pad=(np.inf, pad, pad))
+    print 'Writing subregion with padding of %s to %s...' % (pad, filename)
+    dwi.util.write_subregion_file(filename, bb)
 
 args = parse_args()
 mask = dwi.mask.read_mask(args.input)
 
 selected_slices = list(mask.selected_slices())
 mbb = mask.bounding_box()
-d = dict(basename=args.input, shape=mask.shape, mbb=mbb,
+d = dict(filename=args.input, shape=mask.shape, mbb=mbb,
         mbb_shape=dwi.util.subwindow_shape(mbb), nsel=mask.n_selected(),
         nsl=len(selected_slices), sl=selected_slices)
-print 'mask: {basename}\n'\
+print 'mask: {filename}\n'\
         'minimum bounding box: {mbb_shape}: {mbb}\n'\
         'selected voxels: {nsel}\n'\
         'selected slices: {nsl}: {sl}'.format(**d)
 
 if args.subregion:
-    bb = mask.bounding_box(pad=(np.inf, 10, 10))
-    if args.verbose:
-        print 'Writing subregion to %s...' % args.subregion
-    dwi.util.write_subregion_file(args.subregion, bb)
+    write_subregion(mask, args.pad, args.subregion)
