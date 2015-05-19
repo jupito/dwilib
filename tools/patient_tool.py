78c6b2516251803d4dd6049d330c68a3d377da7d
Jussi Toivonen <jupito@iki.fi>
2015-05-19 19:57:31 +0300

    misc

--- tools/patient_tool.py
+++ tools/patient_tool.py
@@ -3,7 +3,7 @@
 """Patient list tool."""
 
 import argparse
-import collections
+import itertools
 
 import dwi.files
 import dwi.patient
@@ -36,22 +36,40 @@ scores = dwi.patient.get_gleason_scores(patients)
 thresholds = args.thresholds or scores
 label_lesions(patients, thresholds)
 
-print scores
 #for p in patients:
 #    print p
 
 #for p, s, l in dwi.patient.lesions(patients):
 #    print p.num, l.index, l.score, l.label
 
-lesions = reduce(lambda x, y: x+y, (p.lesions for p in patients))
-max_label = max(l.label for l in lesions)
+all_lesions = list(itertools.chain(*(p.lesions for p in patients)))
+n_labels = len({l.label for l in all_lesions})
+max_label = max(l.label for l in all_lesions)
 
-label_groups = collections.defaultdict(list)
-for lesion in lesions:
+label_groups = [[] for _ in range(max_label+1)]
+for lesion in all_lesions:
     label_groups[lesion.label].append(lesion)
 
-for label in sorted(label_groups.keys()):
-    l = label_groups[label]
-    print label, len(l)
-    for lesion in l:
-        print lesion.patient.num, lesion
+group_sizes = [len(l) for l in label_groups]
+sorted_patient_groups = sorted((size, i) for i, size in enumerate(group_sizes))
+for p in patients:
+    for _, i in sorted_patient_groups:
+        if i in [l.label for l in p.lesions]:
+            p.label = i
+            break
+
+for p in patients:
+    print p.num, p.label, [l.label for l in p.lesions]
+
+for i in range(n_labels):
+    print i, sum(1 for p in patients if p.label == i)
+
+
+#for label, lesions in enumerate(label_groups):
+#    print label, len(lesions)
+#    for lesion in lesions:
+#        print lesion.patient.num, lesion
+
+print 'Patients: {}, lesions: {}'.format(len(patients), len(all_lesions))
+print 'Scores: {}: {}'.format(len(scores), scores)
+print 'Group sizes: {}'.format(group_sizes)
