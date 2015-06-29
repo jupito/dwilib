1a8e225912b01f8e3d0b51e58b3e84e195ccea3b
Jussi Toivonen <jupito@iki.fi>
2015-06-29 21:57:12 +0300

    fix

--- dwi/hdf5.py
+++ dwi/hdf5.py
@@ -11,7 +11,8 @@ def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
     f = h5py.File(filename, 'w')
     dset = f.create_dataset(dsetname, data=array, compression='gzip',
             shuffle=True, fletcher32=True)
-    dset.attrs.update(attrs)
+    for k, v in attrs.iteritems():
+        dset.attrs[k] = v
     f.close()
 
 def read_hdf5(filename, dsetname=DEFAULT_DSETNAME):
