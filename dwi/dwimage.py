880840b8cf508d71c734278514106bde257f690c
Jussi Toivonen <jupito@iki.fi>
2015-06-30 18:48:39 +0300

    docs

--- dwi/dwimage.py
+++ dwi/dwimage.py
@@ -9,7 +9,7 @@ import numpy as np
 import dwi.util
 
 def load(filename, nrois=1, varname='ROIdata'):
-    """Load images from a file."""
+    """Load images from a file or directory."""
     root, ext = os.path.splitext(filename)
     if ext == '.mat':
         return load_matlab(filename, varname)
