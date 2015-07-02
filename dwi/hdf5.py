62bbb018d41cdfdf47373e677f89db0dca240d79
Jussi Toivonen <jupito@iki.fi>
2015-07-02 21:10:37 +0300

    docs

--- dwi/hdf5.py
+++ dwi/hdf5.py
@@ -9,6 +9,7 @@ import h5py
 DEFAULT_DSETNAME = 'array'
 
 def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
+    """Write an array as a HDF5 file."""
     f = h5py.File(filename, 'w')
     dset = f.create_dataset(dsetname, data=array, compression='gzip',
             shuffle=True, fletcher32=True)
@@ -17,6 +18,7 @@ def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
     f.close()
 
 def read_hdf5(filename, dsetname=DEFAULT_DSETNAME):
+    """Read an array as a HDF5 file."""
     f = h5py.File(filename, 'r')
     dset = f[dsetname]
     array = np.array(dset)
