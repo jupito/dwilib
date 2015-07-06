2e1777a7f0791a14c07b17cefc8c26e7cb3f4b05
Jussi Toivonen <jupito@iki.fi>
2015-07-06 17:52:40 +0300

    style

--- tools/info.py
+++ tools/info.py
@@ -4,29 +4,21 @@
 
 from __future__ import division, print_function
 import argparse
-import sys
-
-import numpy as np
 
 import dwi.asciifile
 import dwi.util
 
-def parse_args():
-    """Parse command-line arguments."""
-    p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument('input', metavar='FILENAME', nargs='+', default=[],
-            help='input ASCII files')
-    p.add_argument('-v', '--verbose',
-            action='count',
-            help='increase verbosity')
-    p.add_argument('--basic', '-b', action='store_true',
-            help='show median, mean, variance, min, max, sum')
-    p.add_argument('--five', '-f', action='store_true',
-            help='show Tukey\'s five numbers (min, q1, median, q3, max)')
-    args = p.parse_args()
-    return args
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('input', metavar='FILENAME', nargs='+', default=[],
+                    help='input ASCII files')
+parser.add_argument('-v', '--verbose', action='count',
+                    help='increase verbosity')
+parser.add_argument('--basic', '-b', action='store_true',
+                    help='show median, mean, variance, min, max, sum')
+parser.add_argument('--five', '-f', action='store_true',
+                    help="show Tukey's five numbers (min, q1, median, q3, max)")
+args = parser.parse_args()
 
-args = parse_args()
 for filename in args.input:
     af = dwi.asciifile.AsciiFile(filename)
     if args.verbose:
@@ -37,11 +29,10 @@ for filename in args.input:
     for i, a in enumerate(af.a.T):
         d = dict(i=i, param=params[i])
         if args.basic:
+            s = '{i} {param} {mean:g} {std:g} {var:g} {sum:g}'
             d.update(mean=a.mean(), std=a.std(), var=a.var(), sum=sum(a))
-            print('{i}\t{param}'
-                    '\t{mean:g}\t{std:g}\t{var:g}'
-                    '\t{sum:g}'.format(**d))
+            print(s.format(**d))
         if args.five:
+            s = '{i} {param} {min:g} {q1:g} {median:g} {q3:g} {max:g}'
             d.update(dwi.util.fivenumd(a))
-            print('{i}\t{param}'
-                    '\t{min:g}\t{q1:g}\t{median:g}\t{q3:g}\t{max:g}'.format(**d))
+            print(s.format(**d))
