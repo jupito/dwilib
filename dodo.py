525bb7317b8685748c8d07f277c61477f17a9387
Jussi Toivonen <jupito@iki.fi>
2015-07-30 20:37:20 +0300

    moved pathname definitions to new file dwi.path

--- dodo.py
+++ dodo.py
@@ -2,12 +2,14 @@
 
 from __future__ import absolute_import, division, print_function
 from itertools import chain, product
-from os.path import dirname, join
+from os.path import dirname
 
 from doit import get_var
 from doit.tools import check_timestamp_unchanged, create_folder
 
 import dwi.files
+from dwi.paths import (pmap_path, subregion_path, mask_path, roi_path,
+                       texture_path, texture_path_new)
 import dwi.patient
 import dwi.util
 
@@ -100,88 +102,6 @@ def find_roi_param_combinations(mode):
 
 def samplelist_file(mode, samplelist=SAMPLELIST):
     return 'patients_{m.modality}_{l}.txt'.format(m=mode, l=samplelist)
-
-
-def pmap_path(mode, case=None, scan=None):
-    path = dwi.util.sglob('dicoms_{m.model}_*'.format(m=mode), typ='dir')
-    if case is not None and scan is not None:
-        if mode.param == 'raw':
-            # There's no actual parameter, only single 'raw' value (for T2).
-            s = '/{c}_*_{s}*'
-        else:
-            s = '/{c}_*_{s}/{c}_*_{s}*_{m.param}'
-        path += s.format(m=mode, c=case, s=scan)
-    return dwi.util.sglob(path, typ='dir')
-
-
-def subregion_path(mode, case=None, scan=None):
-    path = 'subregions'
-    if case is not None and scan is not None:
-        path += '/{c}_{s}_subregion10.txt'.format(c=case, s=scan)
-    return path
-
-
-def mask_path(mode, masktype, case, scan, lesion=None, algparams=[]):
-    """Return path and deps of masks of different types."""
-    d = dict(m=mode, mt=masktype, c=case, s=scan, l=lesion,
-             ap_='_'.join(algparams))
-    do_glob = True
-    if masktype == 'prostate':
-        path = 'masks_prostate_{m.modality}/{c}_*_{s}*'
-    elif masktype == 'lesion':
-        path = 'masks_lesion_{m.modality}/PCa_masks_{m.modality}_{l}*/{c}_*{s}_*'
-    elif masktype in ('CA', 'N'):
-        path = 'masks_rois/{c}_*_{s}_D_{mt}'
-    elif masktype == 'auto':
-        path = 'masks_auto_{m}/{ap_}/{c}_{s}_auto.mask'
-        do_glob = False  # Don't require existence, can be generated.
-    else:
-        raise Exception('Unknown mask type: {mt}'.format(**d))
-    path = path.format(**d)
-    if do_glob:
-        path = dwi.util.sglob(path)
-    return path
-
-
-def roi_path(mode, masktype, case=None, scan=None, algparams=[]):
-    """Return whole ROI path or part of it."""
-    d = dict(m=mode, mt=masktype, c=case, s=scan, ap_='_'.join(algparams))
-    components = ['rois_{mt}_{m}']
-    if algparams:
-        components.append('{ap_}')
-    if case is not None and scan is not None:
-        components.append('{c}_x_x_{s}_{m.model}_{m.param}_{mt}.txt')
-    components = [x.format(**d) for x in components]
-    return join(*components)
-
-
-def texture_path(mode, case, scan, lesion, masktype, slices, portion,
-                 algparams=()):
-    """Return path to texture file."""
-    path = 'texture_{mt}_{m.model}_{m.param}_{slices}_{portion}'
-    if masktype in ('lesion', 'CA', 'N'):
-        path += '/{c}_{s}_{l}.txt'
-    elif masktype == 'auto':
-        path += '/{ap_}/{c}_{s}_{l}.txt'
-    else:
-        raise Exception('Unknown mask type: {mt}'.format(mt=masktype))
-    return path.format(m=mode, c=case, s=scan, l=lesion, mt=masktype,
-                       slices=slices, portion=portion, ap_='_'.join(algparams))
-
-
-def texture_path_new(mode, case, scan, lesion, masktype, slices, portion,
-                     method, winsize, algparams=()):
-    """Return path to texture file."""
-    path = 'texture_{mt}_{m.model}_{m.param}_{slices}_{portion}'
-    if masktype in ('lesion', 'CA', 'N'):
-        path += '/{c}_{s}_{l}_{ws}-{mth}.txt'
-    elif masktype == 'auto':
-        path += '/{ap_}/{c}_{s}_{l}_{ws}-{mth}.txt'
-    else:
-        raise Exception('Unknown mask type: {mt}'.format(**d))
-    return path.format(m=mode, c=case, s=scan, l=lesion, mt=masktype,
-                       slices=slices, portion=portion, mth=method, ws=winsize,
-                       ap_='_'.join(algparams))
 
 
 def cases_scans(mode):
