7c7b8d09df659dd5dffa10470bf709f420a00654
Jussi Toivonen <jupito@iki.fi>
2015-08-04 01:49:51 +0300

    style

--- dwi/hdf5.py
+++ dwi/hdf5.py
@@ -1,12 +1,13 @@
 """Support for HDF5 files."""
 
-from __future__ import division, print_function
-import collections
+from __future__ import absolute_import, division, print_function
+from collections import OrderedDict
 
 import numpy as np
 import h5py
 
 DEFAULT_DSETNAME = 'array'
+
 
 def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
     """Write an array with attributes into a newly created, compressed HDF5
@@ -19,11 +20,12 @@ def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
         dset.attrs[k] = v
     f.close()
 
+
 def read_hdf5(filename, dsetname=DEFAULT_DSETNAME):
     """Read an array with attributes from an HDF5 file."""
     f = h5py.File(filename, 'r')
     dset = f[dsetname]
     array = np.array(dset)
-    attrs = collections.OrderedDict(dset.attrs)
+    attrs = OrderedDict(dset.attrs)
     f.close()
     return array, attrs
