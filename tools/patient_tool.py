e4680a1cdb9c48223e16f99dad63cec09db28663
Jussi Toivonen <jupito@iki.fi>
2015-08-06 12:02:41 +0300

    style

--- tools/patient_tool.py
+++ tools/patient_tool.py
@@ -2,7 +2,7 @@
 
 """Patient list tool."""
 
-from __future__ import division, print_function
+from __future__ import absolute_import, division, print_function
 import argparse
 import itertools
 
@@ -10,30 +10,33 @@ import dwi.files
 import dwi.patient
 import dwi.util
 
+
 def parse_args():
     """Parse command-line arguments."""
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument('--verbose', '-v', action='count',
-            help='be more verbose')
+                   help='be more verbose')
     p.add_argument('--patients', default='patients.txt',
-            help='patients file')
+                   help='patients file')
     p.add_argument('--thresholds', nargs='*', default=[],
-            help='classification thresholds (group maximums)')
+                   help='classification thresholds (group maximums)')
     p.add_argument('--split', nargs=2, metavar='OUTFILE',
-            help='output train and test files')
+                   help='output train and test files')
     p.add_argument('--ratio', metavar='FLOAT', type=float, default=0.5,
-            help='split ratio')
+                   help='split ratio')
     args = p.parse_args()
     return args
+
 
 def label_lesions(patients, thresholds):
     """Label lesions according to score groups. Also add reference to
     patient."""
-    thresholds = map(dwi.patient.GleasonScore, thresholds)
+    thresholds = [dwi.patient.GleasonScore(t) for t in thresholds]
     for p in patients:
         for l in p.lesions:
             l.label = sum(l.score > t for t in thresholds)
             l.patient = p
+
 
 def label_patients(patients, group_sizes):
     """Label patients according to their lesion labels.
@@ -47,11 +50,13 @@ def label_patients(patients, group_sizes):
                 p.label = i
                 break
 
+
 def group_patients(patients):
     """Group patients by their label."""
     n_labels = len({p.label for p in patients})
     groups = [[p for p in patients if p.label == i] for i in range(n_labels)]
     return groups
+
 
 def random_split(seq, ratio=0.5, surplus=0):
     """Randomly split sequncy into two.
@@ -62,72 +67,78 @@ def random_split(seq, ratio=0.5, surplus=0):
     assert surplus == 0 or surplus == 1
     k = int(ratio * len(seq) + 0.5 * surplus)
     a = random.sample(seq, k)
-    b = [x for x in seq if not x in a]
+    b = [x for x in seq if x not in a]
     return a, b
 
 
-args = parse_args()
-patients = dwi.files.read_patients_file(args.patients, include_lines=True)
-scores = dwi.patient.get_gleason_scores(patients)
-thresholds = args.thresholds or scores
-label_lesions(patients, thresholds)
+def main():
+    args = parse_args()
+    patients = dwi.files.read_patients_file(args.patients, include_lines=True)
+    scores = dwi.patient.get_gleason_scores(patients)
+    thresholds = args.thresholds or scores
+    label_lesions(patients, thresholds)
 
-#for p, s, l in dwi.patient.lesions(patients):
-#    print(p.num, l.index, l.score, l.label)
+    # for p, s, l in dwi.patient.lesions(patients):
+    #     print(p.num, l.index, l.score, l.label)
 
-all_lesions = list(itertools.chain(*(p.lesions for p in patients)))
-n_labels = len({l.label for l in all_lesions})
+    all_lesions = list(itertools.chain(*(p.lesions for p in patients)))
+    n_labels = len({l.label for l in all_lesions})
 
-# Lesions grouped by label.
-label_groups = [[] for _ in range(n_labels)]
-for lesion in all_lesions:
-    label_groups[lesion.label].append(lesion)
+    # Lesions grouped by label.
+    label_groups = [[] for _ in range(n_labels)]
+    for lesion in all_lesions:
+        label_groups[lesion.label].append(lesion)
 
-# Number of lesions in each group.
-group_sizes = [len(l) for l in label_groups]
-label_patients(patients, group_sizes)
+    # Number of lesions in each group.
+    group_sizes = [len(l) for l in label_groups]
+    label_patients(patients, group_sizes)
 
-#for p in patients:
-#    print(p.num, p.label, [l.label for l in p.lesions])
+    # for p in patients:
+    #     print(p.num, p.label, [l.label for l in p.lesions])
 
-# Split data into training set and test set.
-if args.split:
-    patient_groups = group_patients(patients)
-    train, test = [], []
-    for i, g in enumerate(patient_groups):
-        a, b = random_split(g, ratio=args.ratio, surplus=i%2)
-        train += a
-        test += b
-        print('Group: {i}, patients: {n}'.format(i=i, n=len(g)))
-        print('  ', [p.num for p in sorted(a)])
-        print('  ', [p.num for p in sorted(b)])
-    for filename, seq in zip(args.split, [train, test]):
-        d = dict(np=len(seq), nl=sum(len(p.lesions) for p in seq), f=filename)
-        print('Writing {np} patients, {nl} lesions to {f}...'.format(**d))
-        with open(filename, 'w') as f:
-            for p in sorted(seq):
-                f.write('{}\n'.format(p.line))
+    # Split data into training set and test set.
+    if args.split:
+        patient_groups = group_patients(patients)
+        train, test = [], []
+        for i, g in enumerate(patient_groups):
+            a, b = random_split(g, ratio=args.ratio, surplus=i % 2)
+            train += a
+            test += b
+            print('Group: {i}, patients: {n}'.format(i=i, n=len(g)))
+            print('  ', [p.num for p in sorted(a)])
+            print('  ', [p.num for p in sorted(b)])
+        for filename, seq in zip(args.split, [train, test]):
+            d = dict(np=len(seq), nl=sum(len(p.lesions) for p in seq),
+                     f=filename)
+            print('Writing {np} patients, {nl} lesions to {f}...'.format(**d))
+            with open(filename, 'w') as f:
+                for p in sorted(seq):
+                    f.write('{}\n'.format(p.line))
 
-print('Patients: {}, lesions: {}'.format(len(patients), len(all_lesions)))
-print('Scores: {}: {}'.format(len(scores), scores))
-print('Number of lesions in each group: {}'.format(group_sizes))
+    print('Patients: {}, lesions: {}'.format(len(patients), len(all_lesions)))
+    print('Scores: {}: {}'.format(len(scores), scores))
+    print('Number of lesions in each group: {}'.format(group_sizes))
 
-print()
-print('Patients grouped by score group:')
-for i, g in enumerate(label_groups):
-    p = sorted({l.patient.num for l in g})
-    d = dict(i=i, n=len(p), p=p)
-    print('{i}: {n} patients: {p}'.format(**d))
+    print()
+    print('Patients grouped by score group:')
+    for i, g in enumerate(label_groups):
+        pnums = sorted({l.patient.num for l in g})
+        d = dict(i=i, n=len(pnums), p=pnums)
+        print('{i}: {n} patients: {p}'.format(**d))
 
-print()
-print('Patients grouped by number of lesions:')
-min_lesions = min(len(p.lesions) for p in patients)
-max_lesions = max(len(p.lesions) for p in patients)
-for i in range(min_lesions, max_lesions+1):
-    l = [p.num for p in patients if len(p.lesions) == i]
-    print('{i} lesions: {n} patients: {l}'.format(i=i, n=len(l), l=l))
+    print()
+    print('Patients grouped by number of lesions:')
+    min_lesions = min(len(p.lesions) for p in patients)
+    max_lesions = max(len(p.lesions) for p in patients)
+    for i in range(min_lesions, max_lesions+1):
+        l = [p.num for p in patients if len(p.lesions) == i]
+        print('{i} lesions: {n} patients: {l}'.format(i=i, n=len(l), l=l))
 
-print()
-print('Number of patients assigned to each group with bias counter:')
-for i in range(n_labels):
-    print(i, sum(1 for p in patients if p.label == i))
+    print()
+    print('Number of patients assigned to each group with bias counter:')
+    for i in range(n_labels):
+        print(i, sum(1 for p in patients if p.label == i))
+
+
+if __name__ == '__main__':
+    main()
