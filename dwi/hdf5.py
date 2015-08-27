5b470aac059e958ac668142800c48c857e6b516d
Jussi Toivonen <jupito@iki.fi>
2015-08-27 17:27:42 +0300

    if no default dataset found in hdf5 file, use the sole one

--- dwi/hdf5.py
+++ dwi/hdf5.py
@@ -24,6 +24,10 @@ def write_hdf5(filename, array, attrs, dsetname=DEFAULT_DSETNAME):
 def read_hdf5(filename, dsetname=DEFAULT_DSETNAME):
     """Read an array with attributes from an HDF5 file."""
     f = h5py.File(filename, 'r')
+    if dsetname not in f:
+        if len(f.keys()) != 1:
+            raise ValueError('Ambiguous content: {}'.format(filename))
+        dsetname = f.keys()[0]
     dset = f[dsetname]
     array = np.array(dset)
     attrs = OrderedDict(dset.attrs)
