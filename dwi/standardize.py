48a5a4b19f3644f813816052c84d01c9ba9f4348
Jussi Toivonen <jupito@iki.fi>
2015-08-28 21:45:46 +0300

    assert

--- dwi/standardize.py
+++ dwi/standardize.py
@@ -93,7 +93,7 @@ def map_onto_scale(p1, p2, s1, s2, v):
     assert p1 <= p2, (p1, p2)
     assert s1 <= s2, (s1, s2)
     if p1 == p2:
-        assert s1 == s2, (s1, s2)
+        assert s1 == s2, (p1, p2, s1, s2)
         return s1
     f = (v-p1) / (p2-p1)
     r = f * (s2-s1) + s1
