9a867c9e6f648db6ff541295bb68c47734f04db6
Jussi Toivonen <jupito@iki.fi>
2015-09-04 20:03:44 +0300

    refactoring

--- dwi/paths.py
+++ dwi/paths.py
@@ -12,10 +12,10 @@ def samplelist_path(mode, samplelist):
 
 def pmap_path(mode, case=None, scan=None, new=False):
     if new:
-        if case is None and scan is None:
-            return 'images/{m}'.format(m=mode)
-        else:
-            return 'images/{m}/{c}-{s}.h5'.format(m=mode, c=case, s=scan)
+        path = 'images/{m}'
+        if case is not None and scan is not None:
+            path += '/{c}-{s}.h5'
+        return path.format(m=mode, c=case, s=scan)
     path = dwi.util.sglob('dicoms_{m.model}_*'.format(m=mode), typ='dir')
     if case is not None and scan is not None:
         if mode.param == 'raw':
