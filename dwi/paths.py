280151443a1caed160dfc5fa9f5d7bbbfa492f14
Jussi Toivonen <jupito@iki.fi>
2015-09-04 21:01:06 +0300

    use ImageMode items instead of attributes

--- dwi/paths.py
+++ dwi/paths.py
@@ -7,7 +7,7 @@ import dwi.util
 
 
 def samplelist_path(mode, samplelist):
-    return 'patients_{m.modality}_{l}.txt'.format(m=mode, l=samplelist)
+    return 'patients_{m[0]}_{l}.txt'.format(m=mode, l=samplelist)
 
 
 def pmap_path(mode, case=None, scan=None, new=False):
@@ -16,19 +16,19 @@ def pmap_path(mode, case=None, scan=None, new=False):
         if case is not None and scan is not None:
             path += '/{c}-{s}.h5'
         return path.format(m=mode, c=case, s=scan)
-    path = dwi.util.sglob('dicoms_{m.model}_*'.format(m=mode), typ='dir')
+    path = dwi.util.sglob('dicoms_{m[1]}_*'.format(m=mode), typ='dir')
     if case is not None and scan is not None:
-        if mode.param == 'raw':
+        if mode[2] == 'raw':
             # There's no actual parameter, only single 'raw' value (for T2).
             s = '/{c}_*_{s}*'
         else:
-            s = '/{c}_*_{s}/{c}_*_{s}*_{m.param}'
+            s = '/{c}_*_{s}/{c}_*_{s}*_{m[2]}'
         path += s.format(m=mode, c=case, s=scan)
     return dwi.util.sglob(path, typ='dir')
 
 
 def subregion_path(mode, case=None, scan=None):
-    path = 'subregions/{m.modality}'
+    path = 'subregions/{m[0]}'
     if case is not None and scan is not None:
         path += '/{c}_{s}_subregion10.txt'
     return path.format(m=mode, c=case, s=scan)
@@ -40,9 +40,9 @@ def mask_path(mode, masktype, case, scan, lesion=None, algparams=()):
              ap_='_'.join(algparams))
     do_glob = True
     if masktype == 'prostate':
-        path = 'masks_{mt}_{m.modality}/{c}_*_{s}*'
+        path = 'masks_{mt}_{m[0]}/{c}_*_{s}*'
     elif masktype == 'lesion':
-        path = 'masks_{mt}_{m.modality}/PCa_masks_{m.modality}_{l}*/{c}_*{s}_*'
+        path = 'masks_{mt}_{m[0]}/PCa_masks_{m[0]}_{l}*/{c}_*{s}_*'
     elif masktype in ('CA', 'N'):
         path = 'masks_rois/{c}_*_{s}_D_{mt}'
     elif masktype == 'auto':
@@ -66,7 +66,7 @@ def roi_path(mode, masktype, case=None, scan=None, lesion=None, algparams=()):
     if algparams:
         components.append('{ap_}')
     if case is not None and scan is not None:
-        components.append('{c}_x_x_{s}_{m.model}_{m.param}_{mt}.txt')
+        components.append('{c}_x_x_{s}_{m[1]}_{m[2]}_{mt}.txt')
     components = [x.format(**d) for x in components]
     return os.path.join(*components)
 
@@ -74,7 +74,7 @@ def roi_path(mode, masktype, case=None, scan=None, lesion=None, algparams=()):
 def texture_path(mode, case, scan, lesion, masktype, slices, portion, method,
                  winsize, algparams=(), voxel='mean'):
     """Return path to texture file."""
-    path = 'texture/texture_{mt}_{m.model}_{m.param}_{slices}_{portion}_{vx}'
+    path = 'texture/texture_{mt}_{m[1]}_{m[2]}_{slices}_{portion}_{vx}'
     if voxel == 'mean':
         ext = 'txt'
     else:
