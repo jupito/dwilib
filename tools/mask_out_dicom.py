26938f791ad9ee5792f1599409773d7c5f7d174e
Jussi Toivonen <jupito@iki.fi>
2015-09-17 20:43:51 +0300

    style

--- tools/mask_out_dicom.py
+++ tools/mask_out_dicom.py
@@ -2,7 +2,7 @@
 
 """Zero out all voxels in multi-file DICOM image according to a mask."""
 
-from __future__ import division, print_function
+from __future__ import absolute_import, division, print_function
 import argparse
 import collections
 import os
@@ -12,17 +12,19 @@ import dicom
 
 import dwi.mask
 
+
 def parse_args():
     """Parse command-line arguments."""
     p = argparse.ArgumentParser(description=__doc__)
     p.add_argument('-v', '--verbose', action='count',
-            help='increase verbosity')
+                   help='increase verbosity')
     p.add_argument('--image', '-i', metavar='IMAGE', required=True,
-            help='input DICOM image directory')
+                   help='input DICOM image directory')
     p.add_argument('--mask', '-m', metavar='MASK', required=True,
-            help='mask path')
+                   help='mask path')
     args = p.parse_args()
     return args
+
 
 def get_slices(dirname):
     """Return filename lists indexed by slice position."""
@@ -33,7 +35,7 @@ def get_slices(dirname):
     positions = collections.defaultdict(list)
     for pathname in pathnames:
         ds = dicom.read_file(pathname)
-        if not 'PixelData' in ds:
+        if 'PixelData' not in ds:
             continue
         orientation = orientation or ds.ImageOrientationPatient
         if ds.ImageOrientationPatient != orientation:
@@ -41,10 +43,11 @@ def get_slices(dirname):
         shape = shape or ds.pixel_array.shape
         if ds.pixel_array.shape != shape:
             raise Exception("Shape mismatch.")
-        position = tuple(map(float, ds.ImagePositionPatient))
+        position = tuple(float(x) for x in ds.ImagePositionPatient)
         positions[position].append(pathname)
     slices = [positions[k] for k in sorted(positions.keys())]
     return slices
+
 
 def mask_out_slice(mask_slice, pathname):
     """Mask out a slice (set all unselected voxels to zero).
@@ -55,20 +58,25 @@ def mask_out_slice(mask_slice, pathname):
     if mask_slice.shape != ds.pixel_array.shape:
         raise Exception('Slice shape mismatch')
     ds.pixel_array *= mask_slice
-    ds.PixelData = ds.pixel_array.tostring() # Must be written to PixelData.
+    ds.PixelData = ds.pixel_array.tostring()  # Must be written to PixelData.
     ds.save_as(pathname)
 
 
-args = parse_args()
-mask = dwi.mask.read_mask(args.mask)
-slices = get_slices(args.image)
+def main():
+    args = parse_args()
+    mask = dwi.mask.read_mask(args.mask)
+    slices = get_slices(args.image)
 
-if mask.shape()[0] != len(slices):
-    raise Exception('Number of slices mismatch.')
+    if mask.shape()[0] != len(slices):
+        raise Exception('Number of slices mismatch.')
 
-for mask_slice, pathnames, i in zip(mask.array, slices, range(len(slices))):
-    for p in pathnames:
-        if args.verbose:
-            d = dict(i=i, n_selected=np.sum(mask_slice != False), p=p)
-            print('Slice {i:d} ({n_selected:d}): {p:s}'.format(**d))
-        mask_out_slice(mask_slice, p)
+    for mask_slice, paths, i in zip(mask.array, slices, range(len(slices))):
+        for p in paths:
+            if args.verbose:
+                d = dict(i=i, n_selected=np.sum(mask_slice is not False), p=p)
+                print('Slice {i:d} ({n_selected:d}): {p:s}'.format(**d))
+            mask_out_slice(mask_slice, p)
+
+
+if __name__ == '__main__':
+    main()
