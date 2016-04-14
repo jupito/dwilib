dc56257f71741e0075386c0ef5758e70c9f31a70
Jussi Toivonen <jupito@iki.fi>
2016-04-14 03:20:11 +0300

    set some dtypes more correctly

--- old/make_ascii.py
+++ old/make_ascii.py
@@ -37,8 +37,8 @@ def load_data(pmaps, labels, group_ids):
             X.append(x)
             Y.append(label)
             G.append(group_id)
-    X = np.array(X, dtype=float)
-    Y = np.array(Y, dtype=float)
+    X = np.array(X, dtype=np.float)
+    Y = np.array(Y, dtype=np.float)
     return X, Y, G
 
 
