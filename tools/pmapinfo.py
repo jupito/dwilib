d516386b4f340a8f059b8fa77dca2f4a4854b6b2
Jussi Toivonen <jupito@iki.fi>
2016-05-27 03:27:11 +0300

    removed commented lines; moved code blocks

--- tools/pmapinfo.py
+++ tools/pmapinfo.py
@@ -12,21 +12,6 @@ import dwi.files
 import dwi.util
 
 
-def parse_args():
-    p = argparse.ArgumentParser(description=__doc__)
-    p.add_argument('path', nargs='+',
-                   help='input pmap files')
-    p.add_argument('-v', '--verbose', action='count',
-                   help='increase verbosity')
-    p.add_argument('-k', '--keys', default='shape,path',
-                   help='comma-separated keys')
-    return p.parse_args()
-
-
-def shorten(o):
-    return str(o).translate(None, ' \t')
-
-
 class Pmap(object):
     def __init__(self, path):
         self.path = path
@@ -53,42 +38,29 @@ class Pmap(object):
     errors = property(lambda self: len(self._attrs.get('errors', [])))
 
 
+def shorten(o):
+    return str(o).translate(None, ' \t')
+
+
+def parse_args():
+    p = argparse.ArgumentParser(description=__doc__)
+    p.add_argument('path', nargs='+',
+                   help='input pmap files')
+    p.add_argument('-v', '--verbose', action='count',
+                   help='increase verbosity')
+    p.add_argument('-k', '--keys', default='shape,path',
+                   help='comma-separated keys')
+    return p.parse_args()
+
+
 def main():
     args = parse_args()
     keys = args.keys.split(',')
-    # keys = []
-    # for k in args.keys.split(','):
-    #     if k in d:
-    #         keys.append(k)
-    #     else:
-    #         keys.extend(x for x in d.keys() if x.startswith(k))
     for path in args.path:
         pmap = Pmap(path)
         fmt = '{k}={v}' if args.verbose else '{v}'
         fields = (fmt.format(k=x, v=getattr(pmap, x)) for x in keys)
         print(*fields, sep='\t')
-
-        # d = dict(path=path, paramlen=max(len(x) for x in attrs['parameters']))
-        # for i, param in enumerate(attrs['parameters']):
-        #     a = img[..., i]
-        #     # a[a==0] = np.nan
-        #     nans = np.isnan(a)
-        #     if np.any(nans):
-        #         a = a[-nans]
-        #     d.update(param=param, nonnans=a.size/nans.size,
-        #              min=np.min(a), max=np.max(a), mean=np.mean(a),
-        #              median=np.median(a), p='.4')
-        #     s = '{path}  {param:{paramlen}}  {nonnans:{p}%}  {min:{p}f}  {max:{p}f}  {mean:{p}f}  {median:{p}f}'
-        #     #print(s.format(**d))
-        #     print(img.shape, path)
-        #     # if args.basic:
-        #     #     s = '{i} {param} {mean:g} {std:g} {var:g} {sum:g}'
-        #     #     d.update(mean=a.mean(), std=a.std(), var=a.var(), sum=sum(a))
-        #     #     print(s.format(**d))
-        #     # if args.five:
-        #     #     s = '{i} {param} {min:g} {q1:g} {median:g} {q3:g} {max:g}'
-        #     #     d.update(dwi.util.fivenumd(a))
-        #     #     print(s.format(**d))
 
 
 if __name__ == '__main__':
