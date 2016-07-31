b7afbe0be0d0ee2b77d33dfbcf9a0e7d76e784ed
Jussi Toivonen <jupito@iki.fi>
2016-07-31 08:09:42 +0300

    added a TODO comment

--- dwi/files.py
+++ dwi/files.py
@@ -54,6 +54,8 @@ def valid_lines(filename):
     with open(filename, 'rU') as f:
         for line in f:
             line = line.strip()
+            # TODO: Try this one from matplotlib._rc_params_in_file()
+            # strippedline = line.split('#', 1)[0].strip()
             if line and not line.startswith(COMMENT_PREFIX):
                 yield line
 
