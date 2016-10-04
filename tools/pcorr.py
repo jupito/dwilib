1cdea35fe7ec242145a23bfbbadeaf6272308db5
Jussi Toivonen <jupito@iki.fi>
2016-10-04 19:11:06 +0300

    comments

--- tools/pcorr.py
+++ tools/pcorr.py
@@ -25,43 +25,43 @@ def parse_args():
     return p.parse_args()
 
 
-def print_correlations(data, params):
-    """Print correlations."""
-    data = np.asarray(data)
-    indices = range(data.shape[-1])
-    for i, j in product(indices, indices):
-        if i < j:
-            rho, pvalue = scipy.stats.spearmanr(data[:, i], data[:, j])
-            s = 'Spearman: {:8} {:8} {:+1.4f} {:+1.4f}'
-            print(s.format(params[i], params[j], rho, pvalue))
+# def print_correlations(data, params):
+#     """Print correlations."""
+#     data = np.asarray(data)
+#     indices = range(data.shape[-1])
+#     for i, j in product(indices, indices):
+#         if i < j:
+#             rho, pvalue = scipy.stats.spearmanr(data[:, i], data[:, j])
+#             s = 'Spearman: {:8} {:8} {:+1.4f} {:+1.4f}'
+#             print(s.format(params[i], params[j], rho, pvalue))
 
 
-def print_aucs(data, params):
-    """Print ROC AUCs."""
-    data = np.asarray(data)
-    y = data[:, 0]
-    for i in range(1, data.shape[-1]):
-        x = data[:, i]
-        param = params[i]
-        _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
-        s = 'AUC: {p:8} {a:+1.3f}'
-        print(s.format(p=param, a=auc))
+# def print_aucs(data, params):
+#     """Print ROC AUCs."""
+#     data = np.asarray(data)
+#     y = data[:, 0]
+#     for i in range(1, data.shape[-1]):
+#         x = data[:, i]
+#         param = params[i]
+#         _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
+#         s = 'AUC: {p:8} {a:+1.3f}'
+#         print(s.format(p=param, a=auc))
 
 
-def print_aucs_(data, params):
-    """Print ROC AUCs."""
-    data = np.asarray(data)
-    indices = range(data.shape[-1])
-    for i, j in product(indices, indices):
-        if i != j:
-            y, x = data[:, i], data[:, j]
-            py, px = params[i], params[j]
-            try:
-                _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
-            except ValueError:
-                continue
-            s = 'AUC:  {py:8}  {px:8}  {a:+1.3f}'
-            print(s.format(py=py, px=px, a=auc))
+# def print_aucs_(data, params):
+#     """Print ROC AUCs."""
+#     data = np.asarray(data)
+#     indices = range(data.shape[-1])
+#     for i, j in product(indices, indices):
+#         if i != j:
+#             y, x = data[:, i], data[:, j]
+#             py, px = params[i], params[j]
+#             try:
+#                 _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
+#             except ValueError:
+#                 continue
+#             s = 'AUC:  {py:8}  {px:8}  {a:+1.3f}'
+#             print(s.format(py=py, px=px, a=auc))
 
 
 def floor_or_ceil(a, threshold=0.5, out=None):
@@ -88,16 +88,21 @@ def main():
     pmap = np.concatenate(pmaps)
     # print(pmap.shape)
     # print(np.mean(pmap, axis=0))
+
+    # Replace percentages with integer labels according to thresholds.
     for a in pmap:
         a[0] = floor_or_ceil(a[0], args.thresholds[0])
         a[1] = floor_or_ceil(a[1], args.thresholds[1])
+
     i_val = -1  # Last value on row is the intensity.
     d = dict(pt=args.thresholds[0], lt=args.thresholds[1], param=params[i_val])
-    d['ppt'] = np.count_nonzero(pmap[:, 0]) / len(pmap)
+    d['ppt'] = np.count_nonzero(pmap[:, 0]) / len(pmap)  # Prostate per total.
     pmap = pmap[pmap[:, 0] > 0]  # Include only prostate cubes.
-    d['lpp'] = np.count_nonzero(pmap[:, 1]) / len(pmap)
+    d['lpp'] = np.count_nonzero(pmap[:, 1]) / len(pmap)  # Lesion per prostate.
+    # ROC AUC of sample value vs. cube being lesion.
     _, _, d['auc'] = dwi.util.calculate_roc_auc(pmap[:, 1], pmap[:, i_val],
                                                 autoflip=True)
+
     if args.verbose:
         print('# auc p/total l/p p-threshold l-threshold param')
     print('{auc:.3f}  {ppt:.3f}  {lpp:.3f}  {pt}  {lt}  {param}'.format(**d))
