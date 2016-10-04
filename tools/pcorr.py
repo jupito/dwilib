5a81fab5ff8ba95519cf05ca388247b2402c0df3
Jussi Toivonen <jupito@iki.fi>
2016-10-04 21:05:26 +0300

    removed old comments

--- tools/pcorr.py
+++ tools/pcorr.py
@@ -25,53 +25,6 @@ def parse_args():
     return p.parse_args()
 
 
-# def print_correlations(data, params):
-#     """Print correlations."""
-#     data = np.asarray(data)
-#     indices = range(data.shape[-1])
-#     for i, j in product(indices, indices):
-#         if i < j:
-#             rho, pvalue = scipy.stats.spearmanr(data[:, i], data[:, j])
-#             s = 'Spearman: {:8} {:8} {:+1.4f} {:+1.4f}'
-#             print(s.format(params[i], params[j], rho, pvalue))
-
-
-# def print_aucs(data, params):
-#     """Print ROC AUCs."""
-#     data = np.asarray(data)
-#     y = data[:, 0]
-#     for i in range(1, data.shape[-1]):
-#         x = data[:, i]
-#         param = params[i]
-#         _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
-#         s = 'AUC: {p:8} {a:+1.3f}'
-#         print(s.format(p=param, a=auc))
-
-
-# def print_aucs_(data, params):
-#     """Print ROC AUCs."""
-#     data = np.asarray(data)
-#     indices = range(data.shape[-1])
-#     for i, j in product(indices, indices):
-#         if i != j:
-#             y, x = data[:, i], data[:, j]
-#             py, px = params[i], params[j]
-#             try:
-#                 _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
-#             except ValueError:
-#                 continue
-#             s = 'AUC:  {py:8}  {px:8}  {a:+1.3f}'
-#             print(s.format(py=py, px=px, a=auc))
-
-
-# def floor_or_ceil(a, threshold=0.5, out=None):
-#     """Return floor or ceiling based on a minimum threshold."""
-#     if a < threshold:
-#         return np.floor(a, out=out)
-#     else:
-#         return np.ceil(a, out=out)
-
-
 def main():
     args = parse_args()
     tuples = [dwi.files.read_pmap(x) for x in args.pmaps]
@@ -89,22 +42,6 @@ def main():
     if args.verbose > 1:
         print('Means:', np.mean(pmap, axis=0))
         print('Total sample size:', len(pmap))
-
-    # # Replace percentages with integer labels according to thresholds.
-    # for a in pmap:
-    #     a[0] = floor_or_ceil(a[0], args.thresholds[0])
-    #     a[1] = floor_or_ceil(a[1], args.thresholds[1])
-    # prostate = pmap[pmap[:, 0] > 0]  # Samples labeled as prostate.
-
-    # ppt = np.mean(pmap[:, 0])  # Total prostate per image volume.
-    # lpp = np.mean(prostate[:, 1])  # Total lesion per prostate volume.
-
-    # index = -1  # Last value on row is the intensity.
-    # param = params[index]
-    # # ROC AUC of sample value vs. sample being lesion.
-    # y = prostate[:, 1]
-    # x = prostate[:, index]
-    # _, _, auc = dwi.util.calculate_roc_auc(y, x, autoflip=True)
 
     # Sample values are in the last column.
     index = -1
