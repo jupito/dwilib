15eb15aba220b646157048439c0def300db3d46f
Jussi Toivonen <jupito@iki.fi>
2017-02-12 21:29:16 +0200

    refactoring

--- tools/pmapinfo.py
+++ tools/pmapinfo.py
@@ -4,7 +4,6 @@
 
 from __future__ import absolute_import, division, print_function
 
-from pathlib2 import Path
 import numpy as np
 
 import dwi.conf
@@ -12,43 +11,37 @@ import dwi.files
 import dwi.image
 import dwi.util
 
+lambdas = dict(
+    path=lambda x: x.info['path'],
+    name=lambda x: x.info['path'].name,
+    stem=lambda x: x.info['path'].stem,
+    type=lambda x: x.dtype,
+    shape=lambda x: shorten(x.shape),
+    size=lambda x: x.size,
+    mbb=lambda x: shorten(x[x.mbb()].shape),
+    spacing=lambda x: shorten(x.spacing),
 
-class Pmap(object):
-    """Open pmap to request properties from."""
+    finite=lambda x: np.count_nonzero(np.isfinite(x)),
+    nonzero=lambda x: np.count_nonzero(x),
+    neg=lambda x: np.count_nonzero(x < 0),
 
-    def __init__(self, path, **kwargs):
-        self.path = Path(path)
-        self._img = dwi.image.Image.read(str(path), **kwargs)
+    rfinite=lambda x: lambdas['finite'](x) / x.size,
+    rnonzero=lambda x: lambdas['nonzero'](x) / x.size,
+    rneg=lambda x: lambdas['neg'](x) / x.size,
 
-    name = property(lambda self: self.path.name)
-    stem = property(lambda self: self.path.stem)
-    type = property(lambda self: self._img.dtype)
-    shape = property(lambda self: shorten(self._img.shape))
-    size = property(lambda self: self._img.size)
-    mbb = property(lambda self: shorten(self._img[self._img.mbb()].shape))
-    spacing = property(lambda self: shorten(self._img.spacing))
+    sum=lambda x: np.nansum(x),
+    mean=lambda x: np.nanmean(x),
+    std=lambda x: np.nanstd(x),
+    var=lambda x: np.nanvar(x),
+    min=lambda x: np.nanmin(x),
+    median=lambda x: np.nanmedian(x),
+    max=lambda x: np.nanmax(x),
+    five=lambda x: shorten(dwi.util.fivenums(x)),
 
-    finite = property(lambda self: np.count_nonzero(np.isfinite(self._img)))
-    nonzero = property(lambda self: np.count_nonzero(self._img))
-    neg = property(lambda self: np.count_nonzero(self._img < 0))
-
-    rfinite = property(lambda self: self.finite / self.size)
-    rnonzero = property(lambda self: self.nonzero / self.size)
-    rneg = property(lambda self: self.neg / self.size)
-
-    sum = property(lambda self: np.nansum(self._img))
-    mean = property(lambda self: np.nanmean(self._img))
-    std = property(lambda self: np.nanstd(self._img))
-    var = property(lambda self: np.nanvar(self._img))
-    min = property(lambda self: np.nanmin(self._img))
-    median = property(lambda self: np.nanmedian(self._img))
-    max = property(lambda self: np.nanmax(self._img))
-    five = property(lambda self: shorten(dwi.util.fivenums(self._img)))
-
-    errors = property(lambda self: len(self._img.info['attrs'].get('errors',
-                                                                   ())))
-    ce16 = property(lambda self: cast_errors(self._img, np.float16))
-    ce32 = property(lambda self: cast_errors(self._img, np.float32))
+    errors=lambda x: len(x.info['attrs'].get('errors', ())),
+    ce16=lambda x: cast_errors(x, np.float16),
+    ce32=lambda x: cast_errors(x, np.float32),
+    )
 
 
 def cast_errors(a, dtype):
@@ -67,8 +60,7 @@ def shorten(o):
 
 
 def parse_args():
-    available_keys = sorted(x for x in dir(Pmap) if not x.startswith('_'))
-    epilog = 'Available keys: {}'.format(','.join(available_keys))
+    epilog = 'Available keys: {}'.format(','.join(sorted(lambdas.keys())))
     p = dwi.conf.get_parser(description=__doc__, epilog=epilog)
     p.add('path', nargs='+',
           help='input pmap files')
@@ -87,11 +79,11 @@ def main():
     if args.masks:
         mask = dwi.util.unify_masks(dwi.files.read_mask(x) for x in args.masks)
     for path in args.path:
-        pmap = Pmap(path, params=args.params)
+        pmap = dwi.image.Image.read(path, params=args.params)
         if args.masks:
-            pmap._img.apply_mask(mask)
+            pmap.apply_mask(mask)
         fmt = '{k}={v}' if args.verbose else '{v}'
-        fields = (fmt.format(k=x, v=getattr(pmap, x)) for x in keys)
+        fields = (fmt.format(k=x, v=lambdas[x](pmap)) for x in keys)
         print(*fields, sep='\t')
 
 
