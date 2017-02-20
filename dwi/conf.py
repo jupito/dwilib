0fe72246a564c7272201a19ea733bd7776b23dcd
Jussi Toivonen <jupito@iki.fi>
2017-02-20 23:08:42 +0200

    configuration files

--- dwi/conf.py
+++ dwi/conf.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, division, print_function
 import argparse
 import logging
 
+from dwi.files import Path
 import dwi.util
 
 # TODO: Read from file.
@@ -52,11 +53,36 @@ def rcdefaults():
     rcParams.update(rcParamsDefault)
 
 
+def get_config_paths():
+    """Return existing default configuration files."""
+    dirnames = ['/etc/dwilib', '~/.config/dwilib', '.']
+    filename = 'dwilib.cfg'
+    paths = [Path(x).expanduser() / filename for x in dirnames]
+    paths = [x for x in paths if x.exists()]
+    return paths
+
+
+def parse_config(parser):
+    """Parse configuration files."""
+    prefix = parser.fromfile_prefix_chars[0]
+    args = ['{}{}'.format(prefix, x) for x in get_config_paths()]
+    namespace, _ = parser.parse_known_args(args)
+    return namespace
+
+
+def convert_arg_line_to_args(line):
+    """A better replacement for ArgumentParser."""
+    COMMENT_PREFIX = '#'
+    line = line.split(COMMENT_PREFIX, 1)[0].strip()
+    return line.split()
+
+
 def get_parser(**kwargs):
     """Get an argument parser with the usual standard arguments ready. Function
     'add' is added for convenience.
     """
     p = argparse.ArgumentParser(fromfile_prefix_chars='@', **kwargs)
+    p.convert_arg_line_to_args = convert_arg_line_to_args
     p.add = p.add_argument
     p.add('-v', '--verbose', action='count', default=0,
           help='increase verbosity')
@@ -69,3 +95,11 @@ def init_logging(args):
     """Initialize logging."""
     logging.basicConfig(filename=args.logfile,
                         level=dwi.util.get_loglevel(args.loglevel))
+
+
+def parse_args(parser):
+    """Parse args and configuration as well."""
+    namespace = parse_config(parser)
+    args = parser.parse_args(namespace=namespace)
+    dwi.conf.init_logging(args)
+    return args
