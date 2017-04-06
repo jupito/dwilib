dc6f67f1c279a823205ace921e69dc1347ebf608
Jussi Toivonen <jupito@iki.fi>
2017-04-06 16:27:26 +0300

    bring some classes to dwi namespace

--- dwi/__init__.py
+++ dwi/__init__.py
@@ -3,3 +3,6 @@
 from __future__ import absolute_import, division, print_function
 
 from dwi.conf import rcParams
+from dwi.files import Path, PurePath
+from dwi.patient import GleasonScore, Lesion, Patient
+from dwi.util import ImageMode
