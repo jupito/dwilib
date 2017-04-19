1beaa7836344032a0257cace4050d0c33ef5cf61
Jussi Toivonen <jupito@iki.fi>
2017-04-19 21:57:56 +0300

    added dwi.job for tasking & parallelization stuff

---
+++ dwi/job.py
@@ -0,0 +1,40 @@
+"""Functionality related to tasking and parallelization."""
+
+# https://pythonhosted.org/joblib/memory.html
+# https://pythonhosted.org/joblib/parallel.html
+
+# TODO: Configuration.
+
+from functools import partial
+
+import joblib
+
+import dwi
+
+_memory_defaults = dict(
+    # cachedir='cache',
+    cachedir=dwi.rcParams['cachedir'],
+    # mmap_mode=None,
+    # compress=False,
+    verbose=0,
+    # bytes_limit=None,
+    )
+_parallel_defaults = dict(
+    n_jobs=-2,
+    # backend=None,
+    verbose=5,
+    # timeout=None,
+    # pre_dispatch='2 * n_jobs',
+    # batch_size='auto',
+    # temp_folder=None,
+    # max_nbytes='1M',
+    # mmap_mode='r',
+    )
+
+Memory = partial(joblib.Memory, **_memory_defaults)
+
+Parallel = partial(joblib.Parallel, **_parallel_defaults)
+delayed = partial(joblib.delayed)
+
+dump = partial(joblib.dump)
+load = partial(joblib.load)
