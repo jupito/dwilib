9010b5a54b5c71af91e20da3accab02747f213f4
Jussi Toivonen <jupito@iki.fi>
2017-06-14 03:29:01 +0300

    use .types

--- dwi/__init__.py
+++ dwi/__init__.py
@@ -2,7 +2,5 @@
 
 from __future__ import absolute_import, division, print_function
 
-from dwi.conf import rcParams
-from dwi.files import Path, PurePath
-from dwi.patient import GleasonScore, Lesion, Patient
-from dwi.util import ImageMode
+from .conf import rcParams
+from .types import *
