745a6b3e03c937ec014b11c369737f45d112cc6b
Jussi Toivonen <jupito@iki.fi>
2017-07-06 05:39:43 +0300

    refactoring

--- dwi/compat.py
+++ dwi/compat.py
@@ -8,6 +8,45 @@ from . import asciifile, dataset, files, patient
 from .types import Path, TextureSpec
 
 
+def _pmap_path(directory, case, scan, roi=None):
+    directory = Path(directory)
+    d = dict(c=case, s=scan, r=roi)
+    if roi is None:
+        s = '{c}_*{s}*.txt'
+    else:
+        d['r'] += 1
+        s = '{c}_*{s}_{r}*.txt'
+    pattern = s.format(**d)
+    paths = list(directory.glob(pattern))
+    if len(paths) != 1:
+        raise FileNotFoundError(directory / pattern)
+    return paths[0]
+
+
+def _read_pmap(directory, case, scan, roi=None, voxel='all'):
+    """Read single pmap. XXX: Obsolete code."""
+    af = asciifile.AsciiFile(_pmap_path(directory, case, scan, roi=roi))
+    pmap = af.a
+    params = af.params()
+    if pmap.shape[-1] != len(params):
+        # TODO: Move to Asciifile initializer?
+        raise Exception('Number of parameters mismatch: %s' % af.filename)
+    # Select voxel to use.
+    if voxel == 'all':
+        pass  # Use all voxels.
+    elif voxel == 'sole':
+        # Use sole voxel (raise exception if more found).
+        if len(pmap) != 1:
+            raise Exception('Too many voxels: {}'.format(len(pmap)))
+    elif voxel == 'mean':
+        pmap = np.mean(pmap, axis=0, keepdims=True)  # Use mean voxel.
+    elif voxel == 'median':
+        pmap = np.median(pmap, axis=0, keepdims=True)  # Use median.
+    else:
+        pmap = pmap[[int(voxel)]]  # Use single voxel only.
+    return pmap, params, af.filename
+
+
 def read_pmaps(patients_file, pmapdir, thresholds=('3+3',), voxel='all',
                multiroi=False, dropok=False, location=None):
     """Read pmaps labeled by their Gleason score.
@@ -30,8 +69,8 @@ def read_pmaps(patients_file, pmapdir, thresholds=('3+3',), voxel='all',
         case = pat.num
         roi = lesion.index if multiroi else None
         try:
-            pmap, params, pathname = read_pmap(pmapdir, case, scan, roi=roi,
-                                               voxel=voxel)
+            pmap, params, pathname = _read_pmap(pmapdir, case, scan, roi=roi,
+                                                voxel=voxel)
         except IOError:
             if dropok:
                 logging.warning('Cannot read pmap for %s, dropping...',
@@ -48,40 +87,6 @@ def read_pmaps(patients_file, pmapdir, thresholds=('3+3',), voxel='all',
         if params != data[0]['params']:
             raise Exception('Irregular params: %s' % pathname)
     return data
-
-
-def read_pmap(dirname, case, scan, roi=None, voxel='all'):
-    """Read single pmap. XXX: Obsolete code."""
-    d = dict(c=case, s=scan, r=roi)
-    if roi is None:
-        s = '{c}_*{s}*.txt'
-    else:
-        d['r'] += 1
-        s = '{c}_*{s}_{r}*.txt'
-    try:
-        path, = Path(dirname).glob(s.format(**d))
-    except ValueError:
-        raise FileNotFoundError(Path(dirname) / s.format(**d))
-    af = asciifile.AsciiFile(path)
-    pmap = af.a
-    params = af.params()
-    if pmap.shape[-1] != len(params):
-        # TODO: Move to Asciifile initializer?
-        raise Exception('Number of parameters mismatch: %s' % af.filename)
-    # Select voxel to use.
-    if voxel == 'all':
-        pass  # Use all voxels.
-    elif voxel == 'sole':
-        # Use sole voxel (raise exception if more found).
-        if len(pmap) != 1:
-            raise Exception('Too many voxels: {}'.format(len(pmap)))
-    elif voxel == 'mean':
-        pmap = np.mean(pmap, axis=0, keepdims=True)  # Use mean voxel.
-    elif voxel == 'median':
-        pmap = np.median(pmap, axis=0, keepdims=True)  # Use median.
-    else:
-        pmap = pmap[[int(voxel)]]  # Use single voxel only.
-    return pmap, params, af.filename
 
 
 def param_to_tspec(param):
