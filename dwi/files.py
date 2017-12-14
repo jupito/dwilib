bad683b73b3945046f5b3d9f7add6796f7772f2f
Jussi Toivonen <jupito@iki.fi>
2017-12-14 02:35:12 +0200

    temporary suffix fix

--- dwi/files.py
+++ dwi/files.py
@@ -174,7 +174,8 @@ def write_subregion_file(filename, win, comment=''):
 def guess_format(path):
     """Guess file format identifier from it's suffix. Default to DICOM."""
     # return PurePath(path).suffix[1:]
-    suffix = ''.join(PurePath(path).suffixes)[1:]
+    # suffix = ''.join(PurePath(path).suffixes)[1:]  # XXX: Fails eg .bak.h5.
+    suffix = PurePath(path).suffix[1:]  # TODO: Fails on .nii.gz.
     if suffix in ['h5', 'txt', 'zip']:
         return suffix
     if suffix in ['nii', 'nii.gz']:
